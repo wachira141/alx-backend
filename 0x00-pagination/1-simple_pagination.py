d0104d27aa36bbd63bbca5747e8390e227066bc4
wachira140 <wachiraw424@gmail>
2023-05-15 19:35:07 +0300

    add method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,14 +3,14 @@ import csv
 import math
 from typing import List, Tuple
 
-'''
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    
-    return a tuple of size tow with start index and an end index
 
-    page_position = page - 1
-    return (page_position * page_size, page * page_size)
-'''
+#def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    
+ #   return a tuple of size tow with start index and an end index
+
+ #   page_position = page - 1
+ #   return (page_position * page_size, page * page_size)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
