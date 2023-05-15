7485941eb1cc4a2a8580c79d2f8062a76d6abdd9
wachira140 <wachiraw424@gmail>
2023-05-15 19:23:44 +0300

    add class method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,14 +3,14 @@ import csv
 import math
 from typing import List, Tuple
 
-
-def index_range(page: int, page_size: int) -> Tuple:
-    '''
+'''
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    
     return a tuple of size tow with start index and an end index
-    '''
+
     page_position = page - 1
     return (page_position * page_size, page * page_size)
-
+'''
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -30,6 +30,12 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        '''
+        return a tuple of size tow with start index and an end index
+        '''
+        page_position = page - 1
+        return (page_position * page_size, page * page_size)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''
@@ -40,7 +46,7 @@ class Server:
         assert page > 0 and page_size > 0
 
         data = self.dataset()
-        result = index_range(page, page_size)
+        result = self.index_range(page, page_size)
 
         if result[0] > len(data):
             return []
